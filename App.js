514ceb2a150e9e96831741080c92d6d70ca8cef1
SimonielMusyoki <musyoki.mtk3@gmail.com>
2020-12-13 09:25:55 +0300

    Asset & Constants Confs

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {
+  View,
+  Text
+} from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import { FONTS } from './constants'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+
+  let [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'Roboto-Black': require('./assets/fonts/Roboto-Black.ttf'),
+  });
+
+  if(!fontsLoaded){
+    return (
+      <AppLoading />
+    )
+  }
+
+  return(
+    <View>
+      <Text style={{...FONTS.h1}}>Hey There</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ constants/icons.js
@@ -0,0 +1,33 @@
+export const back_arrow = require('../assets/icons/back_arrow_icon.png')
+export const bookmark = require('../assets/icons/bookmark_icon.png')
+export const card = require('../assets/icons/card_icon.png')
+export const claim = require('../assets/icons/claim_icon.png')
+export const clock = require('../assets/icons/clock_icon.png')
+export const dashboard = require('../assets/icons/dashboard_icon.png')
+export const menu = require('../assets/icons/menu_icon.png')
+export const more = require('../assets/icons/more_icon.png')
+export const notification = require('../assets/icons/notification_icon.png')
+export const page_filled = require('../assets/icons/page_filled_icon.png')
+export const page = require('../assets/icons/page_icon.png')
+export const plus = require('../assets/icons/plus_icon.png')
+export const point = require('../assets/icons/point_icon.png')
+export const read = require('../assets/icons/read_icon.png')
+export const search = require('../assets/icons/search_icon.png')
+
+export default {
+    back_arrow,
+    bookmark,
+    card,
+    claim,
+    clock,
+    dashboard,
+    menu,
+    more,
+    notification,
+    page_filled,
+    page,
+    plus,
+    point,
+    read,
+    search
+}
